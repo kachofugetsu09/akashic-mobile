4406e1dc76ad5232825263999d39034d32a78890
huashen <2494946808@qq.com>
2026-07-28 23:39:10 +0800

    chore(release): prepare 0.8.13

--- clients/android/app/build.gradle.kts
+++ clients/android/app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "com.akashic.mobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 33
-        versionName = "0.8.12"
+        versionCode = 34
+        versionName = "0.8.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
