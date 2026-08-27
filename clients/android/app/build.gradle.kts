9a8451131569b3e19d2847f42cdda9055b441845
huashen <2494946808@qq.com>
2026-08-27 21:50:31 +0800

    fix: project shared Session turns on Android

--- clients/android/app/build.gradle.kts
+++ clients/android/app/build.gradle.kts
@@ -62,8 +62,8 @@ android {
         applicationId = "com.akashic.mobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 68
-        versionName = "0.8.33-rc.8"
+        versionCode = 69
+        versionName = "0.8.33-rc.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
